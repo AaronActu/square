0a6e6c03738626c995438ef9472672844e344bd8
AaronActu <aaron.barchechath@gmail.com>
2016-11-23 21:46:25 +0100

    It's running!
    Square is now running but you must edit $hauteur, $largeur to change the height/lenght of your square !

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
 <?php
 
+  const $version = 0.8;
+
+  // On simplifie
+  $hauteur = 5;
+  $largeur = 6;
+
+  if ($increment==1 XOR $increment==2)
+  {
+          // echo "Entrez la hauteur du rectangle : ";
+          //<input>
+          
+          // echo "Entrez la largeur du rectangle : ";
+          //<input>
+
+          for ($increment=0; $increment < $hauteur; $increment++)
+          {
+                  for ($increment2=0; $increment2 < $largeur ; $increment2++)
+                  echo "*";
+                  echo "<br/>";
+          }
+  }
 ?>
